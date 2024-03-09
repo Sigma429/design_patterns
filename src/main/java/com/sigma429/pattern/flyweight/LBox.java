f78fd91a45efd8f471a904a41a33ee049de175f8
Sigma429 <1059769283@qq.com>
2024-03-09 21:28:14 +0800

    Structural pattern

---
+++ src/main/java/com/sigma429/pattern/flyweight/LBox.java
@@ -0,0 +1,14 @@
+package com.sigma429.pattern.flyweight;
+
+/**
+ * @version v1.0
+ * @ClassName: IBox
+ * @Description: L图形类(具体享元角色)
+ * @Author: 黑马程序员
+ */
+public class LBox extends AbstractBox {
+
+    public String getShape() {
+        return "L";
+    }
+}
